d3eeb4cce211f94dd1b6fff4190820cf1ac80eb4
Sam Croyle <sam300385@hotmail.com>
2020-10-08 19:09:28 +0000

    register and login test

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import (
     Flask, flash, render_template,
@@ -58,9 +57,10 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
